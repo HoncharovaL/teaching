f9443212e7e866fb5d3f921b32cb0fbe4f25768c
so-nata <natalie.ivlieva@yandex.ru>
2017-05-28 16:41:10 +0300

    Commit

--- src/AppBundle/Entity/Ad.php
+++ src/AppBundle/Entity/Ad.php
@@ -336,15 +336,6 @@ class Ad
     {
         return $this->value;
     }
-     /**
-     * Get value
-     *
-     * @return string
-     */
-    public function getPhoto()
-    {
-        return $this->getUser()->getPhoto();
-    }
     /**
      * Set dateServ
      *
